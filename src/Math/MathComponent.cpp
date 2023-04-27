cfc82d96417008e800489d91f519ba45c55c2bec
2Ez4Joker <118948467+2Ez4Joker@users.noreply.github.com>
2023-04-28 00:08:36 +0300

    Update MathComponent.cpp

--- src/Math/MathComponent.cpp
+++ src/Math/MathComponent.cpp
@@ -1,12 +1,67 @@
 #include "MathComponent.hpp"
-
+#include<vector>
 #include <iostream>
 
 auto MathComponent::calculate_intersection(Triangle triangle_1,
                                            Triangle triangle_2)
     -> Intersection {
-    // алгоритм здесь
-    // заместо этих двух строк
+   for (int i = 0;i < 2;++i)
+	{
+    for (int j = 0;j < 2;++j)
+		{		
+			bool indicator = true;
+			float x1 = triangle_1[i].x;
+			float y1 = triangle_1[i].y;
+			float x2 = triangle_1[(i+1)%3].x;
+			float y2 = triangle_1[(i+1)%3].y;
+			float x3 = triangle_2[j].x;
+			float y3 = triangle_2[j].y;
+			float x4 = triangle_2[(j + 1) % 3].x;
+			float y4 = triangle_2[(j + 1) % 3].y;
+			if ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4) != 0)
+			{
+				float new_x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+				float new_y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+				if (!(min(x1, x2) <= new_x <= max(x1, x2)))
+				{
+					indicator = false;
+				}
+				if (!(min(y1, y2) <= new_y <= max(y1, y2)))
+				{
+					indicator = false;
+				}
+				if (!(min(x3, x3) <= new_x <= max(x3, x4)))
+				{
+					indicator = false;
+				}
+				if (!(min(y3, y4) <= new_y <= max(y3, y4)))
+				{
+					indicator = false;
+				}
+				if (indicator)
+				{
+					Point2f new_point;
+					new_point.x = new_x;
+					new_point.y = new_y;
+					Intersection.push_back(new_point);
+				}
+			}
+		}
+	}
+	for (int i = 0; i < 3;++i)
+	{
+		if (point_inside_triangle(triangle_2[0], triangle_2[1], triangle_2[2], triangle_1[i]))
+		{
+			Intersection.push_back(triangle_1[i]);
+		}
+	}
+	for (int i = 0; i < 3;++i)
+	{
+		if (point_inside_triangle(triangle_1[0], triangle_1[1], triangle_1[2], triangle_2[i]))
+		{
+			Intersection.push_back(triangle_2[i]);
+		}
+	}
     (void)triangle_1;
     (void)triangle_2;
 
@@ -15,12 +70,20 @@ auto MathComponent::calculate_intersection(Triangle triangle_1,
 
 auto MathComponent::calculate_ratio(Triangle triangle_1, Triangle triangle_2)
     -> float {
-    // алгорим по нахождению коэффа здесь
-    // заместо этих двух строк
+    float max_x = max(abs(triangle_1[0].x), abs(triangle_1[1].x));
+	  max_x = max(max_x, abs(triangle_1[2].x));
+	  max_x = max(max_x, abs(triangle_2[0].x));
+	  max_x = max(max_x, abs(triangle_2[1].x));
+	  max_x = max(max_x, abs(triangle_2[2].x));
+	  float max_y = max(abs(triangle_1[0].y), abs(triangle_1[1].y));
+	  max_y = max(max_y, abs(triangle_1[2].y));
+	  max_y = max(max_y, abs(triangle_2[0].y));
+	  max_y = max(max_y, abs(triangle_2[1].y));
+	  max_y = max(max_y, abs(triangle_2[2].y));
+	  float kaef = 400/max(max_y, max_x);
     (void)triangle_1;
     (void)triangle_2;
-
-    return 1.F;
+    return kaef; 
 }
 
 
